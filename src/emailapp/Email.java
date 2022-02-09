da424ec38f4780fd574a8415d794200da7469dab
RSalcido2019 <ricardo.salcido@outlook.com>
2022-02-09 16:40:03 -0600

    initial set up

---
+++ src/emailapp/Email.java
@@ -0,0 +1,19 @@
+package emailapp;
+import java.util.*;
+import java.io.*;
+
+
+public class Email {
+    public Scanner s = new Scanner(System.in); //Scanner class   this class was obtained from the library java.util.*
+    // Setting variables as private. This prevents direct access
+    private String fname;
+    private String lname;
+    private String dept;
+    private String email;
+    private String password;
+    private int mailCapacity = 500;
+    private String alter_email;
+
+
+
+}
